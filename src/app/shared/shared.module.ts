6230837cb0eb0b63abfaaa4e894b80e7364f7968
Moisés <msportela2@hotmail.com>
2023-05-15 22:39:28 -0300

    ajuste listagem pokemon/ inicio barra search

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,7 @@ import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
 import { PokeSearchComponent } from './poke-search/poke-search.component';
 import { PokeListComponent } from './poke-list/poke-list.component';
+import {RouterLink, RouterModule} from "@angular/router";
 
 
 
@@ -19,9 +20,10 @@ import { PokeListComponent } from './poke-list/poke-list.component';
     PokeSearchComponent,
     PokeListComponent
   ],
-  imports: [
-    CommonModule,
-    NgOptimizedImage
-  ]
+    imports: [
+        CommonModule,
+        NgOptimizedImage,
+        RouterModule
+    ]
 })
 export class SharedModule { }
