f1c0304ca10a98707531eeb6e41d275fb1f10479
Moisés <msportela2@hotmail.com>
2023-05-16 01:30:10 -0300

    faltando search no list component

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -8,29 +8,49 @@ import {take} from "rxjs";
   styleUrls: ['./poke-list.component.scss']
 })
 export class PokeListComponent implements OnInit{
+
+  private setlistaPokemons:{name:string,status:any}[]=[];
   listaPokemons:{name:string,status:any}[]=[];
+  namePokemonFilter:string='';
 
   constructor(
     private pokemonService:PokeApiService
   ) {
   }
   ngOnInit(){
-    this.LoadPokemonUrl()
+    this.loadPokemonUrl()
 
   }
-  LoadPokemonUrl(){
+  loadPokemonUrl(){
     this.pokemonService.listAllPokemons().pipe(take(1)).subscribe((simpleList)=> {
       simpleList=simpleList.results
       for(let pokemon of simpleList){
         this.loadPokemonInfo(pokemon.name,pokemon.url)
       }
-      console.log(this.listaPokemons)
     })
 
   }
   loadPokemonInfo(name:string,url:string){
     this.pokemonService.listStatusPokemons(url).pipe(take(1)).subscribe((info)=>{
-      this.listaPokemons.push({name:name,status:info})
-    })
+      this.setlistaPokemons.push({name:name,status:info})
+      this.listaPokemons=this.setlistaPokemons;
+    });
+  }
+  //Arrumar essa função
+  searchName($event:any){
+    if($event.length>this.namePokemonFilter.length){
+      this.namePokemonFilter=$event;
+    }else{
+      return;
+    }
+    if($event.length>=this.namePokemonFilter.length) {
+      const pokemonNameFilter = this.listaPokemons.filter((resp: any) => {
+        return !resp.name.indexOf($event.toLowerCase());
+      });
+      this.listaPokemons = pokemonNameFilter;
+    }else {
+      this.loadPokemonUrl()
+    }
+
   }
 }
